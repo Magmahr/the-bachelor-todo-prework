b3550955832ea3a8e74780dac54e77c784976d59
Matt Maher <mattmaher0@gmail.com>
2018-05-18 20:33:57 +0000

    Done.

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,19 +1,63 @@
 def get_first_name_of_season_winner(data, season)
-  # code here
+  winner = ""
+  data[season].each do |contestant|
+    if contestant["status"] == "Winner"
+      winner << contestant["name"]
+    end 
+  end  
+  winner.split.first
 end
 
 def get_contestant_name(data, occupation)
-  # code here
+  job = ""
+  data.each do |season, contestant|
+    contestant.each do |attribute|
+      if attribute["occupation"] == occupation
+        job << attribute["name"]
+      end 
+    end
+  end 
+  job
 end
 
 def count_contestants_by_hometown(data, hometown)
-  # code here
+  count = 0
+  data.each do |season, contestant|
+    contestant.each do |attribute|
+      if attribute["hometown"] == hometown
+        count += 1
+      end 
+    end
+  end 
+  count
 end
 
 def get_occupation(data, hometown)
-  # code here
+  job = []
+  data.each do |season, contestant|
+    contestant.each do |attribute|
+      if attribute["hometown"] == hometown
+        job << attribute["occupation"]
+      end 
+    end
+  end 
+  job[0]
 end
 
 def get_average_age_for_season(data, season)
-  # code here
+  age = []
+  contestant_count = 0
+  data[season].each do |contestant|
+      if contestant["age"]
+        age << contestant["age"].to_i
+        contestant_count += 1
+      end
+  end 
+  total = 0.0
+  age.each do |num|
+    total += num
+  end
+  avg_age = total / contestant_count.to_f
+  avg_age.round
+  
 end
